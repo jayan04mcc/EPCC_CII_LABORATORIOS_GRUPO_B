9fb49505af8a17d85530adc489fc28ac268752e6
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2022-04-29 16:20:33 -0500

    Create LAB01_GRUPO_B_EJECUTABLE_1_20204682_JAYAN_CACERES.cpp

---
+++ LAB01_GRUPO_B_EJECUTABLE_1_20204682_JAYAN_CACERES.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"ingrese 2 numeros a operar";
+    cin>>a>>b;
+    c=a*b;
+    cout<<"el resultado es "<<c;
+ }
