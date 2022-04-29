d5a1acd8fa97b5f3968d0c865a8402d48c167a62
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2022-04-29 16:30:40 -0500

    Create LAB01_GRUPO_B_EJECUTABLE_6_20204682_JAYAN_CACERES.cpp

---
+++ LAB01_GRUPO_B_EJECUTABLE_6_20204682_JAYAN_CACERES.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+#include <cmath>
+using namespace std;  
+int main(){
+  int v[50], x, i=0;
+	cout<<"ingrese un num para pasarlo a binario: ";
+	cin>>x;
+	do{
+		v[i]=x%2;
+		x=x/2;
+		i++;
+	} while(x!=0);
+	for (int t=i;t<50;t++){
+		v[t]=9;
+	}
+	for (i=49;i>=0;i--){
+		if(v[i]==0)cout<<v[i];
+		if(v[i]==1)cout<<v[i];
+	}
+}
