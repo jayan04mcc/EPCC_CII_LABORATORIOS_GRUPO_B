c56483642e389bf1a2d0a607d1c8bdf5e4782e8a
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2022-04-29 16:21:59 -0500

    Create LAB01_GRUPO_B_EJECUTABLE_2_20204682_JAYAN_CACERES.cpp

---
+++ LAB01_GRUPO_B_EJECUTABLE_2_20204682_JAYAN_CACERES.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+char nomb[15],app[15],apm[15];
+    cout<<"ingrese su nombre";
+    cin>>nomb;
+    cout<<"ingrese el apellido paterno";
+    cin>>app;
+    cout<<"ingrese su apellido materno";
+    cin>>apm;
+        cout<<app;
+    for (int z=0;z<=1;z++){
+        cout<<apm[z];
+    }
+    cout<<"@unsa.edu.pe";
+}
