9719d55833b955d76db5e03d24b8ac33ebd08b76
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2022-05-03 20:58:04 -0500

    Update LAB04_GRUPO_B_EJECUTABLE_4_20204682_JAYAN_CACERES.cpp

--- LAB04_GRUPO_B_EJECUTABLE_4_20204682_JAYAN_CACERES.cpp
+++ LAB04_GRUPO_B_EJECUTABLE_4_20204682_JAYAN_CACERES.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 int main(){
-  int matriz[5][3];
-int sum=0;
-for(int i=0;i<5;i++){
-    for(int j=0;j<3;j++){
-        cout<<"posicion ["<<i<<"]["<<j<<"]"<<endl;
-        cin>>matriz[i][j];
-        if(i%2==0){
-            sum=sum+matriz[i][j];
-
+int primos[25];
+ for(int i=2;i<=100;i++){
+        int cont=0;
+        for(int j=2;j<=i/2;j++){
+            if(i%j==0){
+            cont++;
+            break;
+            }
+        }
+        if (cont<1){
+        cout<<i<<endl;
+        cin>>primos[i];
         }
     }
-}
-cout<<"la suma es"<<sum;
 }
